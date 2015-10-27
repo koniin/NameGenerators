06ba388934df2d80acd9d892f8a953e9e215b4dd
Henrik Aronsson <arnssn@gmail.com>
2015-10-27 20:22:10 +0100

    Repository setup

---
+++ NameGenerators/SimpleCharacterNameGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NameGenerators {
+    public class SimpleCharacterNameGenerator {
+        static string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+        static string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+        public static string GenerateName(Random r, int nameLength) {
+            string name = "";
+            for(int i = 0; i < nameLength; i++) {
+                name += i % 2 == 0 ? consonants[r.Next(consonants.Length)] : vowels[r.Next(vowels.Length)];
+            }
+            return name;
+        }
+    }
+}
---
+++ NameGenerators/SpaceBodyGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NameGenerators {
+    public class SpaceBodyGenerator {
+        static string[] planetPrefix = { "X", "A", "Z", "T", "F", "Q" };
+        static string[] planetSuffix = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        public static string GenerateSpaceBodyName(Random r, int lettersLength, int numbersLength) {
+            string name = "", letter = "";
+            while(name.Length < lettersLength) {
+                letter = planetPrefix[r.Next(planetPrefix.Length)];
+                if(!name.Contains(letter))
+                    name += letter;
+            }
+            while(name.Length < lettersLength + numbersLength) {
+                letter = planetSuffix[r.Next(planetSuffix.Length)];
+                name += letter;
+            }
+            return name;
+        }
+    }
+}
